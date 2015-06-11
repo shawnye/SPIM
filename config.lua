0f348d8649153721d120b7e6f9b04415be572092
shawnye <xyye.shawn@gmail.com>
2015-06-11 13:40:25 +0800

    config.lua as lua_preload_file
    other config changes

--- config.lua
+++ config.lua
@@ -1,2 +1,11 @@
 package.path =   mg.document_root .. "/lua/?.lua;" .. package.path
 
+template = require "resty.template"
+template.caching(false) --for debug
+template.document_root(mg.document_root)
+--template render to
+template.print = function(s)
+--mg.write("-----------------------")
+  mg.write(s)
+--mg.write("-----------------------")
+end
