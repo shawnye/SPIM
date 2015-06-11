740f533b8561388efcf22cd90988ce178d40bd5e
shawnye <xyye.shawn@gmail.com>
2015-06-11 13:40:51 +0800

    upate download test

--- www/lua/download.lua
+++ www/lua/download.lua
@@ -1,30 +1,14 @@
-require"config"
-
-
-local template = require "resty.template"
-template.caching(false) --for debug
-template.document_root(mg.document_root)
---template render to
-template.print = function(s)
---mg.write("-----------------------")
-  mg.write(s)
---mg.write("-----------------------")
-end
-
-
+--require"config" @see lua_proload_file
 
 local file = mg.get_var(mg.request_info.query_string, "file");
 if not file then
-    local data = {
-      message = "文件ID为空"
-    }
-
-    --local page =  t:merge(data)
 
     local u = require"mgutils"
-    mg.write (u.getHeader(400) )
+    u.writeHeader(400)
 
-    template.render( "error.html", data )
+    template.render( "error.html", {
+      message = "文件ID为空"
+    })
 
 
 else
--- www/lua/mgutils.lua
+++ www/lua/mgutils.lua
@@ -33,7 +33,42 @@ function M.getHeader( statusCode )
     return table.concat( bf )
 end
 
-statusCode=400
-print(statusCode, getStatus(tostring(statusCode)))
+function M.writeHeader( statusCode )
+  mg.write (M.getHeader(statusCode) )
+end
+
+function M.isPost()
+  return mg.request_info.request_method:upper() == "POST"
+end
+
+function M.isFileUpload()
+  return M.isPost() and mg.request_info.content_type:lower():sub(1,19) == 'multipart/form-data'
+end
+
+function M.getHeader( key )
+  if not key then
+    return nil
+  end
+  return mg.request_info.http_headers[key]
+end
+
+function M.readPostData()
+-- Read the entire body data (POST content) into "stringtab" variable.
+-- Use a string builder pattern for performance reasons
+local stringtab = {}
+
+repeat
+  local add_data = mg.read()
+  if add_data then
+    stringtab[#stringtab+1] = add_data
+  end
+until (add_data == nil);
+
+return table.concat(stringtab)
+
+end
+
+--statusCode=400
+--print(statusCode, getStatus(tostring(statusCode)))
 
 return M
